48301c4568a6175c768785ceb8abfcdfa41d3457
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-09-02 13:19:00 -0700

    app commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// this allows to stash "artificial env variables" in a file
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+console.log(process.env.PORT)
+console.log(process.env.API_SECRET)
+
+const PORT = process.env.PORT || 5000 // fallback is nice
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+  res.send(`<h1>UNIT 4 Today's Project</h1>`)
+})
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'Web 45 is awesome!' })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
